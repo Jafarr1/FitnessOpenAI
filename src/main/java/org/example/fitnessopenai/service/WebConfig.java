0919e92a0de87da61480f0e89c30f46f48a29847
jafar <jaal0002@stud.kea.dk>
2024-12-09 14:08:18 +0100

    corsconfiguration

--- src/main/java/org/example/fitnessopenai/service/WebConfig.java
+++ src/main/java/org/example/fitnessopenai/service/WebConfig.java
@@ -13,11 +13,11 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // Allow CORS for all paths
-                        .allowedOrigins("https://ashy-moss-01592bf10.5.azurestaticapps.net")  // Allow only your frontend domain
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow specific methods
-                        .allowedHeaders("*")  // Allow all headers
-                        .allowCredentials(true);  // Allow credentials (cookies, etc.)
+                registry.addMapping("/**")
+                        .allowedOrigins("https://ashy-moss-01592bf10.5.azurestaticapps.net")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
